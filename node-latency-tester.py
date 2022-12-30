1840fdfa07cefd1ef3f38c6074e6cf295ade5df6
supranodes <121648634+supranodes@users.noreply.github.com>
2022-12-30 16:49:33 +0100

    Update node-latency-tester.py

--- node-latency-tester.py
+++ node-latency-tester.py
@@ -35,9 +35,9 @@ print ("")
 print ("Enter the amount of request to make for the test")
 num_calls = int(input("(Public nodes: 100-150 // Private Node, 800-1000) : "))
 print ("")
-print(colored(0, 191, 255,"######################################################"))
+
 print(colored(0, 191, 255,"#############     www.supranodes.com     #############"))
-print(colored(0, 191, 255,"######################################################"))
+
 for endpoint in endpoints:
     request_times[endpoint] = []
     if endpoint.startswith("http"):
@@ -63,9 +63,9 @@ for endpoint, times in request_times.items():
 
     print(f"Average latency time for {endpoint} is  {colored(0, 255, 0, '{:.3f}'.format(average_time * 1000))} ms")
 
-print(colored(0, 191, 255,"######################################################"))
+
 print(colored(0, 191, 255,"#############     www.supranodes.com     #############"))
-print(colored(0, 191, 255,"######################################################"))
+
 
 print("")
 print("")
